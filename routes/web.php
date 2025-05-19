af44aad2dbfeae00e409ae927db231a0c1230236
vvduth <ducthai060501@gmail.com>
2025-05-19 11:29:34 +0300

    feat: add Content Security Policy configuration and middleware integration

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use App\Http\Controllers\CurrencyConverterController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Csp\AddCspHeaders;
 
 Route::get('/', function () {
     return view('welcome', ['name' => 'Laravel1']);
-});
+})->middleware(AddCspHeaders::class);
 
+Route::get('/token', function () {
+    return csrf_token();
+})->middleware(AddCspHeaders::class);
+
+
+Route::get('/test', function () {
+    return response('Test CSP');
+});
